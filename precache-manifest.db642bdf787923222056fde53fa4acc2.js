46b4971f783cd66ee358c6498f27fd3c6fd806dd
Viktoriia <jhunt@ukr.net>
2019-10-30 15:00:31 +0200

    Updates

---
+++ precache-manifest.db642bdf787923222056fde53fa4acc2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "badff777b6db317504b35083e43b9a9c",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "ee38ebe66f975df5165e",
+    "url": "/react_tabs/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "bb7e4e5bb3dbc6c5e1e3",
+    "url": "/react_tabs/static/js/2.7b369de4.chunk.js"
+  },
+  {
+    "revision": "ee38ebe66f975df5165e",
+    "url": "/react_tabs/static/js/main.4a81ee39.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
